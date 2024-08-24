8b3c5826d89833d0ed6f6abb8e62133ff631df79
yamamara <yamamrtyu@gmail.com>
2024-08-24 19:03:30 -0400

    Added verification functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import atexit
 import base64
 import json
 import os
+import random
 import signal
 import sys
 
@@ -14,8 +15,45 @@ load_dotenv()
 
 guild_id = os.getenv("GUILD_ID")
 bot = commands.Bot("!", intents=discord.Intents.all())
+verification_channel_id = int(os.getenv("VERIFICATION_CHANNEL_ID"))
 leaderboard_channel_id = int(os.getenv("LEADERBOARD_CHANNEL_ID"))
+unverified_role_id = int(os.getenv("UNVERIFIED_ROLE_ID"))
 secret_words = []
+
+
+# Custom "Buttons" class that executes code on click instead of only URL
+class Buttons(discord.ui.View):
+    def __init__(self, *, timeout=180, message):
+        super().__init__(timeout=timeout)
+        self.message = message
+
+    # Removes unverified role of user on click of "Accept" button
+    @discord.ui.button(label="Accept", style=discord.ButtonStyle.green)
+    async def accept_button(self, interaction, button):
+        # Cross-checks author ID with member ID since DM message returns "User" object
+        for member in bot.get_all_members():
+            if member.id == self.message.author.id:
+                guild = bot.get_guild(int(guild_id))
+                member = guild.get_member(member.id)
+                await member.remove_roles(guild.get_role(unverified_role_id))
+
+        # Disables both "Accept" and "Deny" buttons
+        for button in self.children:
+            button.disabled = True
+
+        embed = interaction.message.embeds[0]
+        await interaction.response.edit_message(embed=embed, view=self)
+        await interaction.response.defer()
+
+    # Disables both buttons on click of "Disable" button
+    @discord.ui.button(label="Deny", style=discord.ButtonStyle.red)
+    async def deny_button(self, interaction, button):
+        for button in self.children:
+            button.disabled = True
+
+        embed = interaction.message.embeds[0]
+        await interaction.response.edit_message(embed=embed, view=self)
+        await interaction.response.defer()
 
 
 def on_kill(*args):
@@ -30,6 +68,10 @@ def is_owner(interaction):
     return interaction.user.id == int(os.getenv("OWNER_ID"))
 
 
+def is_dm(interaction):
+    return isinstance(interaction.channel, discord.channel.DMChannel)
+
+
 def get_leaderboard_embed(description):
     return discord.Embed(title=os.getenv("LEADERBOARD_TITLE"), color=0x000000, description=description)
 
@@ -38,6 +80,21 @@ def get_leaderboard_embed(description):
 def serialize_leaderboard():
     with open("leaderboard.json", "w") as outfile:
         json.dump(leaderboard, outfile)
+
+
+def get_number_with_suffix(number):
+    if 10 <= number % 100 <= 19:
+        suffix = "th"
+    elif number % 10 == 1:
+        suffix = "st"
+    elif number % 10 == 2:
+        suffix = "nd"
+    elif number % 10 == 3:
+        suffix = "rd"
+    else:
+        suffix = "th"
+
+    return number + suffix
 
 
 async def add_to_leaderboard(message):
@@ -58,7 +115,8 @@ async def add_to_leaderboard(message):
         await channel_message.edit(embed=get_leaderboard_embed(description))
 
     await message.channel.send(
-        f"Congratulations {message.author.mention} 🥳! You are the {leaderboard[str(message.author.id)]}th person "
+        f"Congratulations {message.author.mention} 🥳! "
+        f"You are the {get_number_with_suffix(leaderboard[str(message.author.id)])}th person "
         f"{os.getenv('CONGRATULATIONS_MESSAGE')}"
     )
 
@@ -80,9 +138,7 @@ async def print_leaderboard(interaction):
         await bot.get_channel(leaderboard_channel_id).send(embed=get_leaderboard_embed(""))
         await interaction.response.send_message("Printed empty leaderboard to leaderboard channel!")
     else:
-        await interaction.response.send_message(
-            f"You do not have permission to run that command! {os.getenv('PERMISSION_MESSAGE')}"
-        )
+        await interaction.response.send_message(os.getenv('PERMISSION_MESSAGE'))
 
 
 @bot.tree.command(name="printrules", description="Prints the rules", guild=discord.Object(id=guild_id))
@@ -106,8 +162,13 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = await member.create_dm()
-    await channel.send(f"Welcome to the server {member.mention}!")
+    # Makes members unverified on join
+    await member.add_roles(member.guild.get_role(unverified_role_id))
+
+    # Sends DM to new members with welcome messages
+    dm = await member.create_dm()
+    await dm.send(os.getenv("WELCOME_MESSAGE_1"))
+    await dm.send(os.getenv("WELCOME_MESSAGE_2"))
 
 
 # Runs on every message sent in any channel
@@ -117,9 +178,23 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    for word in secret_words:
-        if word in message.content:
-            await add_to_leaderboard(message)
+    # Handles verification response if sent from DM
+    if is_dm(message):
+        verification_channel = bot.get_channel(verification_channel_id)
+
+        # Sends DM response in verification channel as embed with random color
+        await verification_channel.send(embed=discord.Embed(
+            title=f"{message.author.mention} appears",
+            color=random.randint(1, 16777215),
+            description=message.content
+        ), view=Buttons(message=message))
+
+        await message.channel.send(os.getenv("VERIFICATION_MESSAGE"))
+    # Only checks for secret word in server
+    else:
+        for word in secret_words:
+            if word in message.content:
+                await add_to_leaderboard(message)
 
 
 # Creates "leaderboard.json" file if it doesn't exist
