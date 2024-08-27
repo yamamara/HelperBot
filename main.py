4652f823351dc7fdf6ecc0fca52d789f2759751c
yamamara <yamamrtyu@gmail.com>
2024-08-26 22:02:24 -0400

    Changed empty leaderboard and rules command from slash to text

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ class Buttons(discord.ui.View):
         await interaction.response.defer()
 
 
+# Runs exit code on OS signals instead of immediately killing
 def on_kill(*args):
     sys.exit(0)
 
@@ -64,8 +65,8 @@ def does_file_exist(file_path):
     return os.path.isfile(file_path) and os.path.getsize(file_path) > 0
 
 
-def is_owner(interaction):
-    return interaction.user.id == int(os.getenv("OWNER_ID"))
+def is_owner(message_author):
+    return message_author.id == int(os.getenv("OWNER_ID"))
 
 
 def is_dm(interaction):
@@ -76,8 +77,20 @@ def get_leaderboard_embed(description):
     return discord.Embed(title=os.getenv("LEADERBOARD_TITLE"), color=0x000000, description=description)
 
 
+# Adds user leaderboard data to returned description string
+def get_leaderboard_string():
+    description = ""
+
+    for user_id in leaderboard:
+        description += f"<@{user_id}>: {leaderboard[user_id]}\n"
+
+    return description
+
+
 # Serializes updated and distinct leaderboard to "leaderboard.json"
 def serialize_leaderboard():
+    print(f"Serializing leaderboard: {leaderboard}")
+
     with open("leaderboard.json", "w") as outfile:
         json.dump(leaderboard, outfile)
 
@@ -98,21 +111,15 @@ def get_number_with_suffix(number):
 
 
 async def add_to_leaderboard(message):
-    description = ""
-
-    # Adds one to user count for message author
+    # Adds one to leaderboard count for message author
     if str(message.author.id) not in leaderboard:
         leaderboard.update({str(message.author.id): 1})
     else:
         leaderboard[str(message.author.id)] += 1
 
-    # Adds user leaderboard data to embed description string
-    for user_id in leaderboard:
-        description += f"<@{user_id}>: {leaderboard[user_id]}\n"
-
     # Updates first message in leaderboard channel with updated and formatted leaderboard
     async for channel_message in bot.get_channel(leaderboard_channel_id).history(oldest_first=True, limit=1):
-        await channel_message.edit(embed=get_leaderboard_embed(description))
+        await channel_message.edit(embed=get_leaderboard_embed(get_leaderboard_string()))
 
     await message.channel.send(
         f"Congratulations {message.author.mention} 🥳! "
@@ -131,25 +138,15 @@ async def ping(interaction):
         f"{os.getenv('PING_MESSAGE')}\n```Latency: {round(bot.latency * 1000)}ms```")
 
 
-@bot.tree.command(name="printleaderboard", description="Prints an empty leaderboard", guild=discord.Object(id=guild_id))
-async def print_leaderboard(interaction):
-    # Only allows owner to print leaderboard
-    if is_owner(interaction):
-        await bot.get_channel(leaderboard_channel_id).send(embed=get_leaderboard_embed(""))
-        await interaction.response.send_message("Printed empty leaderboard to leaderboard channel!")
-    else:
-        await interaction.response.send_message(os.getenv('PERMISSION_MESSAGE'))
+@bot.tree.command(name="leaderboard", description="Prints the current leaderboard", guild=discord.Object(id=guild_id))
+async def leaderboard(interaction):
+    # Adds user and count data from leaderboard into embed
+    await interaction.response.send_message(embed=get_leaderboard_embed(get_leaderboard_string()))
 
 
-@bot.tree.command(name="printrules", description="Prints the rules", guild=discord.Object(id=guild_id))
-async def print_rules(interaction):
-    # Prints rules in rules channel if command sent by owner
-    if is_owner(interaction):
-        rules_channel = bot.get_channel(int(os.getenv("RULES_CHANNEL_ID")))
-        await rules_channel.send(os.getenv("RULES"))
-        await interaction.response.send_message("Printed rules to rules channel!")
-    else:
-        await interaction.response.send_message(os.getenv("RULES"))
+@bot.tree.command(name="rules", description="Prints the rules", guild=discord.Object(id=guild_id))
+async def rules(interaction):
+    await interaction.response.send_message(os.getenv("RULES"))
 
 
 # First function ran when client connects
@@ -158,6 +155,7 @@ async def on_ready():
     # Makes commands accessible by users in server
     await bot.tree.sync(guild=discord.Object(id=guild_id))
     print(f"Logged into server as: {bot.user}")
+    print(f"Current leaderboard: {leaderboard}")
 
 
 @bot.event
@@ -190,8 +188,23 @@ async def on_message(message):
         ), view=Buttons(message=message))
 
         await message.channel.send(os.getenv("VERIFICATION_MESSAGE"))
-    # Only checks for secret word in server
     else:
+        # Only allows owner to print empty leaderboard and rules in respective channels
+        if message.content == "owner print leaderboard":
+            if is_owner(message.author):
+                await bot.get_channel(leaderboard_channel_id).send(embed=get_leaderboard_embed(""))
+                await message.channel.send("Printed empty leaderboard to leaderboard channel!")
+            else:
+                await message.channel.send(os.getenv('PERMISSION_MESSAGE'))
+        elif message.content == "owner print rules":
+            if is_owner(message.author):
+                rules_channel = bot.get_channel(int(os.getenv("RULES_CHANNEL_ID")))
+                await rules_channel.send(os.getenv("RULES"))
+                await message.channel.send("Printed rules to rules channel!")
+            else:
+                await message.channel.send(os.getenv("PERMISSION_MESSAGE"))
+
+        # Only checks for secret word in server not DMs
         for word in secret_words:
             if word in message.content:
                 await add_to_leaderboard(message)
